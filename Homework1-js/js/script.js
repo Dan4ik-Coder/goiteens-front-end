a15fc5accc50131221f171742a539cfd757c55fa
Dan4ik-Coder <dplay7240@gmail.com>
2021-01-20 12:49:09 +0300

    ДЗ по JS

---
+++ Homework1-js/js/script.js
@@ -0,0 +1,33 @@
+// Завдання 1
+let str = "Привіт"
+let num = 123
+let flag = true
+let txt = "true"
+
+console.log(typeof str)
+console.log(typeof num)
+console.log(typeof flag)
+console.log(typeof txt)
+
+// Завдання 2
+
+let age = 14
+const name = "Vitalik"
+const dogName = "Sharik"
+const boy = true
+const youngerSisterName = "Natasha"
+const olderBrotherName = "Georgy"
+let hobby = "CS:GO"
+let school = "№45"
+let dinner = "paella"
+let dream = "Front-End Developer Senjor"
+
+// А внизу міні-розповідь)
+
+// console.log("My name is " + name + " and I am " + 
+// age + ", I have a dog, his name " + dogName +
+//  " also I have sister " + youngerSisterName + " and brother "
+//  + olderBrotherName + " I have hobby, I really like playing " + hobby
+//  + " I'm study in a school " + school + ", but I want leave from that" 
+//  + "today I have " + dinner + " for dinner " 
+//  + "I really want to be " + dream)
