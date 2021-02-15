c80954b307edecb986021b0ddd05dc2b96a9ff0b
Dan4ik-Coder <dplay7240@gmail.com>
2021-02-15 17:51:40 +0300

    Bugfix Hw4, Practice 2,3

---
+++ Practice2-js/js/script.js
@@ -0,0 +1,75 @@
+'use strict'
+//1 завдання
+const userName = prompt('Ваше ім’я?');
+
+console.log(userName);
+
+//2 завдання
+
+const myAge = 13.5;
+const friendAge = 15;
+
+alert(myAge + friendAge);
+
+console.log('=========================');
+//3 завдання
+
+let a = 6;
+let b = 2;
+
+console.log(a*b);
+
+console.log('=========================');
+//4 завдання
+
+let a1 = 5+3;
+let a2 = 5-3;
+let a3 = 5*3;
+let a4 = 5/3;
+let a5 = 5-3;
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+console.log(a5);
+
+//5 завдання
+
+
+let a6 = 5%3;
+let a7 = 3%5;
+let a8 = 5+'3';
+let a9 = '5'-3;
+let a10 = 75+'кг';
+
+alert(a6);
+alert(a7);
+alert(a8);
+alert(a9);
+alert(a10);
+
+console.log('=========================');
+//6 завдання
+
+const height = 23;
+const width = 10;
+const s = height*width;
+console.log(s);
+
+console.log('=========================');
+//7 завдання
+
+const heightC = 10;
+const dC = 4;
+const p = 3.14;
+const v = heightC*dC*p;
+console.log(v);
+
+console.log('=========================');
+//8 завдання
+
+const n = 3;
+const m = 4;
+const k = n**2+m**2;
+console.log(k)
---
+++ Practice3-js/js/script.js
@@ -0,0 +1,71 @@
+'use strict'
+
+//1 завдання
+
+let k1 = '5px';
+let k2 = '12djd334';
+let k3 = '12.45asdwe12';
+let k4 = 'qwqweeewq';
+
+console.log(Number.parseInt(k1));
+console.log(Number.parseInt(k2));
+console.log(Number.parseInt(k3));
+console.log(Number.parseInt(k4));
+
+console.log('================');
+
+//2 завдання
+
+console.log(Number.parseFloat(k1));
+console.log(Number.parseFloat(k2));
+console.log(Number.parseFloat(k3));
+console.log(Number.parseFloat(k4));
+
+console.log('================');
+
+//3 завдання
+
+console.log(Math.min(2, 34, 99, 3 , 22, 36, 733, 18));
+console.log(Math.max(2, 34, 99, 3 , 22, 36, 733, 18));
+
+console.log('================');
+
+//4 завдання
+
+console.log(Math.random() * (19 - 3) + 1);
+
+console.log('================');
+
+//5 завдання
+
+const result = 5+5+'5';
+console.log(result);
+console.log(typeof(result));
+
+console.log('================');
+
+//6 завдання
+
+const email = 'dplay7240@gmail.com';
+console.log(email.length)
+console.log(email.includes('@'))
+
+console.log('================');
+
+//7 завдання
+
+//*w1 від word1
+const w1 = 'My';
+const w2 = 'name';
+const w3 = 'is';
+let fullName = `${w1} ${w2} ${w3}`;
+const w4 = 'Victor';
+console.log(fullName + ` ${w4}`)
+
+console.log('================')
+
+//8 завдання
+
+const userName = 'Олександра';
+const payment = '300 гривень';
+alert(`Дякуємо, ${userName}! До сплати ${payment}`)
